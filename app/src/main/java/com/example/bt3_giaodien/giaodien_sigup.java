2071d9c61977a8256867e32862afd0e4ad9986e6
Hoang12342906 <104152244+Hoang12342906@users.noreply.github.com>
2022-09-12 18:42:07 +0700

    commit 1

--- app/src/main/java/com/example/bt3_giaodien/giaodien_sigup.java
+++ app/src/main/java/com/example/bt3_giaodien/giaodien_sigup.java
@@ -2,36 +2,29 @@ package com.example.bt3_giaodien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class giaodien_sigup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button btnOpen=(Button)
-                findViewById(R.id.bt_backdangki);
-        btnOpen.setOnClickListener(new
+        setContentView(R.layout.activity_giaodien2);
+        Button btnBack= (Button)
+                findViewById(R.id.bt_backdangnhap);
+        btnBack.setOnClickListener(new
                                            View.OnClickListener() {
-                                               public void onClick(View arg0) {
-                                                   doOpenChildActivity();
+                                               public void onClick(View v) {
+                                                   finish();
                                                }
                                            });
     }
-    public void doOpenChildActivity()
-    {
-        Intent myIntent=new Intent(this, giaodien_sigup.class);
-        startActivity(myIntent);
-    }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.activity_giaodien2, menu);
         return true;
 
     }
--- app/src/main/java/com/example/bt3_giaodien/profile.java
+++ app/src/main/java/com/example/bt3_giaodien/profile.java
@@ -2,37 +2,30 @@ package com.example.bt3_giaodien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
-        Button btnOpen=(Button)
-                findViewById(R.id.bt_backdangki);
-        btnOpen.setOnClickListener(new
+        Button btnBack= (Button)
+                findViewById(R.id.bt_back);
+        btnBack.setOnClickListener(new
                                            View.OnClickListener() {
-                                               public void onClick(View arg0) {
-                                                   doOpenChildActivity();
+                                               public void onClick(View v) {
+                                                   finish();
                                                }
                                            });
     }
-    public void doOpenChildActivity()
-    {
-        Intent myIntent=new Intent(this, giaodien_sigup.class);
-        startActivity(myIntent);
-    }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.profile, menu);
         return true;
-
     }
 }
